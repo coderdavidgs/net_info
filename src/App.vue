4f2de90165063f3eda2f8196124b49a0a07981dc
coderdavidgs <davidgbrdsantos@gmail.com>
2023-11-25 17:13:46 -0300

    create navbar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+    <nav>
+      <div class="nav-items">
+        <router-link to="/" class="nav-text">Home</router-link>
+
+        <router-link to="/" class="nav-text">About</router-link>
+
+        <router-link to="/" class="nav-text">
+          <span class="nav-logo">
+            <img src="./assets/imgs/logo.png" alt="net info">
+            netINFO
+          </span>
+        </router-link>
+
+        <router-link to="/" class="nav-text">Tests</router-link>
+
+        <router-link to="/" class="nav-text">Contact</router-link>
+      </div>
+    </nav>
   <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+ @import url('./assets/css/style.css');
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav>
+      <router-link to="/" class="nav-item">Home</router-link>
+    </nav>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'NavBar',
+    });
+</script>
